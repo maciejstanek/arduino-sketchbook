bfd80529de61b863d77cfb875b953abaf09529cb
maciej-stanek <makaron28@gmail.com>
2017-05-22 10:06:23 +0200

    cnc wip

--- CNC_00/CNC_00.ino
+++ CNC_00/CNC_00.ino
@@ -3,47 +3,75 @@
 
 int pinButton = 2;
 
+// Stepper motors for XY axis
 int stepsPerRev = 20;
 Stepper stepperY(stepsPerRev, 5, 6, 4, 7);
 Stepper stepperX(stepsPerRev, 8, 9, 10, 11);
 
+// Servo for raising/lowering tool
 int pinServo = 3;
 int pinServoKnob = A0;
 int servoHigh = 110;
 int servoLow = 150;
 Servo servo;
 
-int pinJoystickX = A5;
-int pinJoystickY = A4;
-
+// Status data
 int currentX = 0;
 int currentY = 0;
-int targetX = 0;
-int targetY = 0;
+int dirX = 0;
+int dirY = 0;
+int zx = 0;
+int zy = 0;
+boolean manual = true;
+char cmd = '-';
+int status = 0;
+
+int pinJoystickX = A5;
+int pinJoystickY = A4;
 int autoServoAngle = servoHigh;
 int servoAngle = servoHigh;
-boolean manual = true;
 int modeStatusLED = 12;
 int pinManualOverride = 17; // A3 as a digital pin
-
 int pinZeroX = 15; // A1 as a digital pin
 int pinZeroY = 16; // A2 as a digital pin
 
+void printStatusJSON() {
+  Serial.print("{\"c\":{\"x\":");
+  Serial.print(currentX);
+  Serial.print(",\"y\":");
+  Serial.print(currentY);
+  Serial.print("},\"d\":{\"x\":");
+  Serial.print(dirX);
+  Serial.print(",\"y\":");
+  Serial.print(dirY);
+  Serial.print("},\"z\":{\"x\":");
+  Serial.print(zx);
+  Serial.print(",\"y\":");
+  Serial.print(zy);
+  Serial.print("},\"man\":");
+  Serial.print(manual);
+  Serial.print(",\"cmd\":\"");
+  Serial.print(cmd);
+  Serial.print("\",\"sta\":");
+  Serial.print(status);
+  Serial.print("}\n");
+}
+
 void resetOrigin() {
-  Serial.println("Resetting origin...");
-  Serial.println("Doing Y axis...");
+	status = 1;
+	printStatusJSON();
   while(!digitalRead(pinZeroY)) {
     stepperY.step(-1);
   }
   currentY = 0;
-  Serial.println("Y axis done");
-  Serial.println("Doing X axis...");
+	status = 2;
+	printStatusJSON();
   while(!digitalRead(pinZeroX)) {
     stepperX.step(-1);
   }
   currentX = 0;
-  Serial.println("X axis done");
-  Serial.println("Resetting origin finished");
+	status = 0;
+	printStatusJSON();
 }
 
 void setup() {
@@ -62,11 +90,12 @@ void setup() {
 }
 
 void loop() {
+	status = 0;
   
   int knobTuning = map(analogRead(pinServoKnob), 0, 1023, -20, 20);
   servoAngle = digitalRead(pinButton) ? servoHigh : servoLow + knobTuning;
 
-  char cmd = 0;
+  cmd = '-';
   int autoDirX = 0;
   int autoDirY = 0;
   if(Serial.available() > 0) {
@@ -103,14 +132,12 @@ void loop() {
       autoServoAngle = servoLow + knobTuning;
       break;
     case 'R': // reset origin
-      resetOrigin();
+      resetOrigin(); // It's a blocking function, consider moving it outside this logic
       break;
   }
 
   int valJoystickX = analogRead(pinJoystickX);
   int valJoystickY = analogRead(pinJoystickY);
-  int dirX = 0;
-  int dirY = 0;
   if(manual) {
     dirX = valJoystickX < 447 ? 1 : (valJoystickX > 575 ? -1 : 0);
     dirY = valJoystickY < 447 ? -1 : (valJoystickY > 575 ? 1 : 0);
@@ -120,6 +147,16 @@ void loop() {
   }
   
   servo.write(manual ? servoAngle : autoServoAngle);
+  
+  zx = digitalRead(pinZeroX);
+  zy = digitalRead(pinZeroY);
+  
+  if(zx && dirX < 0) {
+    dirX = 0;
+  }
+  if(zy && dirY < 0) {
+    dirY = 0;
+  }
 
   if(dirY) {
     currentY += dirY;
@@ -129,31 +166,8 @@ void loop() {
     currentX += dirX;
     stepperX.step(dirX);
   }
-  
-  Serial.print("{zX=");
-  Serial.print(digitalRead(pinZeroX));
-  Serial.print("} {zY=");
-  Serial.print(digitalRead(pinZeroY));
-  Serial.print("} ");
-  
-  Serial.print("[");
-  Serial.print(dirX);
-  Serial.print(",");
-  Serial.print(dirY);
-  Serial.print("] (");
-  Serial.print(currentX);
-  Serial.print(",");
-  Serial.print(currentY);
-  Serial.print(")->(");
-  Serial.print(targetX);
-  Serial.print(",");
-  Serial.print(targetY);
-  Serial.print(") manual=");
-  Serial.print(manual);
-  Serial.print(" cmd='");
-  Serial.print(cmd);
-  Serial.println("'");
-  
+
+	printStatusJSON(); 
   delay(10);
   
 }

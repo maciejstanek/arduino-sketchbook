a1f99a5c9a19414f0ac854e6cba340d86155d354
maciejstanek <makaron28@gmail.com>
2017-10-26 22:59:14 +0200

    Cleanup base64 image converter.

--- SmartLamp/base64.php
+++ SmartLamp/base64.php
@@ -1,7 +1,3 @@
 #!/usr/bin/php
 <?php
-
-$im = base64_encode(file_get_contents('icon.png')); 
-//echo '<img width="16" height="16">data:image/png;base64,'.$imdata.'</>';
-//echo '<img src="data:image/png;base64,' . $im . '"/>';
-echo $im;
+echo base64_encode(file_get_contents('icon.png')); 
